0748b39c59d47e4e71e8d13703d3c5f35541c382
kryptocodes <srivatsantb@gmail.com>
2023-10-22 19:41:45 +0530

    test: window obj
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/pages/[username].tsx
+++ src/pages/[username].tsx
@@ -123,7 +123,9 @@ export default function Home() {
   const Link = (data:any) => {
     return (
       <button className="flex flex-col db-border items-center py-[12px]  w-full"
-        onClick={() => {}}
+        onClick={() => 
+          window.open(window.location.href , "_blank")
+        }
       >
         <h1 className="flex items-center text-[18px] font-semibold">
           {/* <svg
