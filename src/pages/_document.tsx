1ed84ce3caa28dc15bfb13bd3753cad76f72a5f9
kryptocodes <srivatsantb@gmail.com>
2023-11-15 21:02:37 +0530

    feat: added google analytics
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import Script from 'next/script'
 
 export default function Document() {
   return (
@@ -7,10 +8,28 @@ export default function Document() {
         title='Webtree'
         
       />
+      
       <body>
         <Main />
         <NextScript />
       </body>
+      <Script
+        src="https://accounts.google.com/gsi/client"
+        strategy="lazyOnload"
+      />
+      <Script
+        strategy="lazyOnload"
+        src="https://www.googletagmanager.com/gtag/js?id=G-RSBV84JS2B"
+      />
+      <Script strategy="lazyOnload" id="G-RSBV84JS2B">
+        {`
+            window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-RSBV84JS2B');
+            `}
+      </Script>
+
     </Html>
   )
 }
